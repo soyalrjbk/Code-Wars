1ee843efea7045c9d32dd6fec73a8c1ca7e2745f
De.bug <soyal@Mac.lan>
2026-09-03 11:42:06 -0400

    Solved reverse sequence of array challenge

---
+++ 8 kyu/reverse-sequence-of-array.js
@@ -0,0 +1,16 @@
+/*
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+*/
+
+const reverseSeq = n => {
+  let result=[]
+  if (n>0){
+  for (i=n; i>=1; i--){
+    result.push(i);
+  }
+    }
+  else return ("Error!");
+  return result;
+};
